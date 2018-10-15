a2ef015bbf2782421dbdc328c3391d7c5a972dfb
sunhaipeng2 <sunhaipeng@jd.com>
2018-10-15 22:13:24 +0800

    微信授权 事件总线

---
+++ src/interceptor/interceptors.js
@@ -0,0 +1,69 @@
+// 统一拦截器
+export default {
+  login: {
+    // 发出请求时的回调函数
+    // config (p) {
+    //   // 对所有request请求中的OBJECT参数对象统一附加时间戳属性
+    //   p.timestamp = +new Date();
+    //   // 必须返回OBJECT参数对象，否则无法发送请求到服务端
+    //   return p;
+    // },
+
+    // 请求成功后的回调函数
+    success (p) {
+      // 可以在这里对收到的响应数据对象进行加工处理
+      // 必须返回响应数据对象，否则后续无法对响应数据进行处理
+      return p;
+    },
+
+    //请求失败后的回调函数
+    fail (p) {
+      // 必须返回响应数据对象，否则后续无法对响应数据进行处理
+      wx.showToast({
+        title: "登录异常,请稍后重试",
+        icon: "none",
+      })
+      return p;
+    },
+
+    // 请求完成时的回调函数(请求成功或失败都会被执行)
+    complete (p) {
+    }
+  },
+  request: {
+    // 发出请求时的回调函数
+    config (p) {
+      // 对所有request请求中的OBJECT参数对象统一附加时间戳属性
+      let url = p.url;
+      if (!url.includes("login")) {
+        let session = this.getFromStorageSync(this.globalData.userInfoSessionKey);
+        console.log(session)
+        p.header = {sessionId: session.sessionId}
+      }
+      // 必须返回OBJECT参数对象，否则无法发送请求到服务端
+      return p;
+    },
+
+    // 请求成功后的回调函数
+    success (p) {
+      // 可以在这里对收到的响应数据对象进行加工处理
+      // 必须返回响应数据对象，否则后续无法对响应数据进行处理
+      return p;
+    },
+
+    //请求失败后的回调函数
+    fail (p) {
+      console.log('request fail: ', p);
+      // 必须返回响应数据对象，否则后续无法对响应数据进行处理
+      wx.showToast({
+        title: "服务器异常,请稍后重试",
+        icon: "none",
+      })
+      return p;
+    },
+
+    // 请求完成时的回调函数(请求成功或失败都会被执行)
+    complete (p) {
+    }
+  }
+}
--- src/mixins/user-info.js
+++ src/mixins/user-info.js
@@ -1,36 +1,72 @@
 import wepy from 'wepy'
 
-export default class UserInfoMixin extends wepy.mixin {
+export default class extends wepy.mixin {
   data = {
-    mixin: 'This is mixin data.',
-    userInfoStorageKey: "userInfo"
   }
   methods = {
 
   }
-  getFromStorage(key, successFuc) {
-    wx.getStorage({
-      key: key,
+
+  getUserInfo() {
+    this.$invoke('zanDialog', 'showZanDialog', {
+      title: '认证',
+      content: '请先认证信息',
+      buttons:[
+        {
+          text: '取消',
+          type: 'cancel'
+        },
+        {
+          text: '授权',
+          type: 'xxxx',
+          opentype: "getUserInfo"
+        }
+      ]
+    }).then((e) => {
+      //把信息存到本地缓存里
+      this.$parent.setUserInfoToStorage(this.$parent.globalData.userInfoStorageKey, e.detail.userInfo)
+    }).catch(() => {
+      console.log('=== dialog ===', 'type: cancel')
+    })
+  }
+  getUserSetting() {
+    var _this = this;
+    wx.getSetting({
       success(res) {
-        console.log("成功从本地存储读取数据" + JSON.stringify(res))
-        successFuc(res.data)
-      },
-      fail(res) {
-        console.log("从本地存储读取数据失败" + JSON.stringify(res))
-        successFuc(null)
+        let authSetting = res.authSetting;
+        if (authSetting["scope.userInfo"]) {
+          // 1.已经授权过默认获取用户信息
+          _this.getUserInfoSlience()
+        }else {
+          // 2.如果没有需要授权
+          _this.getUserInfo()
+        }
       }
     })
   }
-  setToStorage(key, data) {
-    wx.setStorage({
-      key: key,
-      data: data,
+
+  getUserInfoSlience() {
+    var _this = this;
+    wx.getUserInfo({
       success(res) {
-        console.log("存储调用成功" + JSON.stringify(res))
-      },
-      fail(res) {
-        console.log("存储调用失败" + JSON.stringify(res))
+        console.log("静默获取用户信息:" + JSON.stringify(res.userInfo))
+        _this.$parent.setUserInfoToStorage(_this.$parent.globalData.userInfoStorageKey, res.userInfo);
       }
     })
+  }
+
+  onLoad() {
+    var _this = this;
+    // 1. 先检查缓存中存不存在
+    _this.$parent.getFromStorage(_this.$parent.globalData.userInfoStorageKey,(res) => {
+      if (res == null) {
+        console.log("未授权开始执行授权处理")
+        // 获取授权设置
+        _this.getUserSetting()
+      }else {
+        console.log("已有授权信息进行登录")
+        _this.$parent.globalData.eventBus.emit("getuser");
+      }
+    });
   }
 }
